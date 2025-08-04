1a251d389cb9d42271d5d72d1998bce07a9b650f
ZynXx <techigwe@gmail.com>
2025-08-04 12:54:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import pygame
 pygame.init()
 
+WINDOW_WIDTH, WINDOW_HEIGTH = 1280, 720
+display_surface = pygame.dispay.set_mode(WINDOW_WIDTH, WINDOW_HEIGTH))
+pygame.display.set_caption("space shooter")
+running = true
+
+while running:
+  for event in pygame.event.get():
+    if event type == pygame.QUIT:
+    running = false
+display_surface.fill('bleu')
+pyga,e.display.update()
+pygame.quit()
