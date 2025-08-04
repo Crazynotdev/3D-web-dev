0dba10087053e1531ff4de06b4cbc13ca7881986
ZynXx <techigwe@gmail.com>
2025-08-04 13:45:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from os.path import join
 pygame.init()
 
 WINDOW_WIDTH, WINDOW_HEIGTH = 1280, 720
@@ -10,14 +11,17 @@ surf = pygame.surface(100,200)
 surf.fill('orange')
 x = 100
 # importer img du player
-player_surf = pygame.image.load('../imgs/player.png')
-
+player_surf = pygame.image.load(join('../imgs/player.png')).convert_alpha()
+star_surf = pygame.image.load(join('../imgs/star.png')).convert_alpha()
+star_positions = [(x,y) for i in range(20)]
 while running:
   for event in pygame.event.get():
     if event type == pygame.QUIT:
     running = false
 display_surface.fill('bleu')
 x += 0.2
-display_surface.blit((surf, x,150))
+display_surface.blit(player_surf, x,150)
+for i in range(20):
+display_surface.blit(star_surf, randit(0, WINDOW_WIDTH),randit(0, WINDOW_HEIGTH))
 pygame.display.update()
 pygame.quit()
